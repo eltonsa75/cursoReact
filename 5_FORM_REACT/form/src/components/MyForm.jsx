78b04d5582aaa21c3ebfb207c69133e673ba24f2
Elton Andrade <eltonsa75@hotmail.com>
2025-03-29 13:03:26 -0300

    Criando uma função para o campo nome

--- 5_FORM_REACT/form/src/components/MyForm.jsx
+++ 5_FORM_REACT/form/src/components/MyForm.jsx
@@ -1,23 +1,39 @@
+import { useState } from "react";
 import "./MyForm.css";
 
 const MyForm = () => {
+  // 3 - gerenciamento de dados
+
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  console.log(name);
   return (
     <div>
       {/* 1 - criação do form */}
       <form>
         <div>
-            <label htmlFor="name">Nome:</label>
-            <input type="text" name="name" placeholder="Digite o seu nome" />
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite o seu nome"
+            onChange={handleName}
+          />
         </div>
         {/* 2 - label envolvendo input */}
         <label>
-            <span>E-mail</span>
-            <input type="text" name="email" placeholder="Digite o seu e-mail" />
+          <span>E-mail</span>
+          <input type="text" name="email" placeholder="Digite o seu e-mail" />
         </label>
         <input type="submit" value="Enviar" />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default MyForm
+export default MyForm;
