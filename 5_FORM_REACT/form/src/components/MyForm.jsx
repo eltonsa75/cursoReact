e9dba1b00932fad05000da14744081f368eac861
Elton Andrade <eltonsa75@hotmail.com>
2025-03-29 13:38:13 -0300

    Criando componentes do Formulário

--- 5_FORM_REACT/form/src/components/MyForm.jsx
+++ 5_FORM_REACT/form/src/components/MyForm.jsx
@@ -7,15 +7,35 @@ const MyForm = () => {
   const [name, setName] = useState();
   const [email, setEmail] = useState();
 
+  const [bio, setBio] = useState("");
+
+  const [role, setRole] = useState("");
+
   const handleName = (e) => {
     setName(e.target.value);
   };
 
-  console.log(name);
+  // 5 envio de form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, bio, role);
+
+    // validacao
+    // envio
+
+    // 7 - limpar form
+    setName("");
+    setEmail("");
+    setBio("");
+    setRole("");
+  };
+
+  console.log(name, email);
   return (
     <div>
       {/* 1 - criação do form */}
-      <form>
+      {/* 5 - envio de formulário */}
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Nome:</label>
           <input
@@ -23,13 +43,39 @@ const MyForm = () => {
             name="name"
             placeholder="Digite o seu nome"
             onChange={handleName}
+            // 6 - controlled input
+            value={name || ""}
           />
         </div>
         {/* 2 - label envolvendo input */}
         <label>
           <span>E-mail</span>
-          <input type="text" name="email" placeholder="Digite o seu e-mail" />
+          <input
+            type="text"
+            name="email"
+            placeholder="Digite o seu e-mail"
+            // 4- Simplicando a manipulação
+            onChange={(e) => setEmail(e.target.value)}
+            // 6 - controlled input
+            value={email || ""}
+          />
         </label>
+        {/* 8 - textarea */}
+        <label>
+          <span>Bio:</span>
+          <textarea
+            name="bio"
+            placeholder="Descrição do Usuário"
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+          ></textarea>
+        </label>
+        <span>Função no sistema</span>
+        <select name="role" onChange={(e) => setRole(e.target.value)} value={role}>
+            <option value="user">Usuário</option>
+            <option value="editor">Editor</option>
+            <option value="admin">Admin</option>
+        </select>
         <input type="submit" value="Enviar" />
       </form>
     </div>
