43ea3ee9400a7379f43ca686ab9ce59f8fb5f9ea
Elton Andrade <eltonsa75@hotmail.com>
2025-04-05 12:13:51 -0300

    Resgatando dados do Http no React

--- 6_HTTP_REACT/http_react/src/App.jsx
+++ 6_HTTP_REACT/http_react/src/App.jsx
@@ -1,12 +1,38 @@
 
+import { useState, useEffect } from "react";
 import './App.css'
 
+// para checar erros, deixar URL vazia
+const url = "http://localhost:3000/products";
+
 function App() {
-  
+  //1 resgatando dados
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    getData();
+  }, []);
+
 
   return (
    <div className='App'>
-    <h2>Http React</h2>
+    <h1>Http React</h1>
+    {/* 1 - Resgate de dados */}
+    <ul>
+      {products.map((products) => (
+        <li key={products.id}>
+          {products.name} - R${products.price}
+        </li>
+      ))}
+    </ul>
    </div>
   )
 }
